a40a3a3e8be676a6e7c6d8ac51a44d5dd2695c42
toopooriik <is511723@gmail.com>
2024-05-23 23:19:58 +1000

    последний штрих

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,6 +1,7 @@
 const allowedCors = [
     'http://localhost:3000',
-    'http://localhost:3001'
+    'http://localhost:3001',
+    "https://front-toporik.nomoredomainswork.ru"
 ]
 
 function cors(req, res, next) {
