f719848d1e6a4373ccd134ab3ae1328c8caa9864
toopooriik <is511723@gmail.com>
2024-05-23 23:36:41 +1000

    перекопировал

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,18 +1,14 @@
-const allowedCors = [
-    'http://localhost:3000',
-    'http://localhost:3001',
-    "https://front-toporik.nomoredomainswork.ru"
-]
+const allowedCors = ["https://pindie-frontend.nomoredomainswork.ru"];
 
 function cors(req, res, next) {
-    const { origin } = req.headers;
-    
-    if (allowedCors.includes(origin)) { 
-        res.header('Access-Control-Allow-Origin', origin);
-    }
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-    next();
+  const { origin } = req.headers;
+
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  next();
 }
 
 module.exports = cors;
