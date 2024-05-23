b1f9ed79f75afbd78310ce2dd045a5871d96607d
toopooriik <is511723@gmail.com>
2024-05-24 01:02:49 +1000

    sd

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,8 +3,11 @@ const allowedCors = ["https://front-toporik.nomoredomainswork.ru"];
 function cors(req, res, next) {
   const { origin } = req.headers;
 
-    res.header("Access-Control-Allow-Origin", origin, "Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE", "Access-Control-Allow-Headers", 
-    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
   next();
 }
 
