42f67a35378af1da935860a7988dbe380fca013d
toopooriik <is511723@gmail.com>
2024-05-23 23:59:08 +1000

    ы

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,11 +3,10 @@ const allowedCors = ["https://pindie-frontend.nomoredomainswork.ru"];
 function cors(req, res, next) {
   const { origin } = req.headers;
 
-  if (allowedCors.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
-  }
   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  res.header("Access-Control-Allow-Headers", 
+  "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
   next();
 }
 
